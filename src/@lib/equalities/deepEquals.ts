398b00f0b2bf31377fb152d9df968d4991b3dfdd
codeming <hmk2000015@gmail.com>
2025-04-05 23:04:57 +0900

    Update deepEquals.ts

--- src/@lib/equalities/deepEquals.ts
+++ src/@lib/equalities/deepEquals.ts
@@ -20,9 +20,26 @@ export function deepEquals<T>(objA: T, objB: T): boolean {
   }
 
   // 일반 객체 처리
-  return Object.keys(objA as object).every(
-    (key) =>
-      Object.prototype.hasOwnProperty.call(objB, key) &&
-      deepEquals((objA as any)[key], (objB as any)[key])
+  const entriesA = Object.entries(objA as object).sort(([keyA], [keyB]) =>
+    keyA.localeCompare(keyB)
   );
+  const entriesB = Object.entries(objB as object).sort(([keyA], [keyB]) =>
+    keyA.localeCompare(keyB)
+  );
+
+  // 객체의 키-값 쌍 개수가 다르면 false 반환
+  if (entriesA.length !== entriesB.length) {
+    return false;
+  }
+
+  // 객체의 각 키-값 쌍을 재귀적으로 비교
+  for (let i = 0; i < entriesA.length; i++) {
+    // 값이 다르면 false 반환
+    if (!deepEquals(entriesA[i][1], entriesB[i][1])) {
+      return false;
+    }
+  }
+
+  // 모든 키-값 쌍이 같으면 true 반환
+  return true;
 }
