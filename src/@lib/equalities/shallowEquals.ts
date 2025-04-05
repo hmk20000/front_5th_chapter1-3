fe8a368bde1cc34d952298aba84e39ec001addb7
codeming <hmk2000015@gmail.com>
2025-04-06 00:39:39 +0900

    Update shallowEquals.ts

--- src/@lib/equalities/shallowEquals.ts
+++ src/@lib/equalities/shallowEquals.ts
@@ -1,35 +1,49 @@
 export function shallowEquals<T>(objA: T, objB: T): boolean {
   /**
-   * Array인 경우 처리
+   * 기본 타입 비교
    */
-  if (Array.isArray(objA) && Array.isArray(objB)) {
-    for (let i = 0; i < objA.length; i++) {
-      if (objA[i] !== objB[i]) {
-        return false;
-      }
-    }
+  if (Object.is(objA, objB)) {
     return true;
   }
+
   /**
-   * Object인 경우 처리
+   * 오브젝트 비교
    */
-  if (typeof objA === 'object' && typeof objB === 'object') {
-    // null인 경우 처리 [ null 도 오브젝트다 ]
-    if (objA === null || objB === null) {
-      return objA === objB;
-    }
-
-    const keysA = Object.keys(objA);
-
-    // 얕은 비교. 1차원 레벨만 비교
-    for (let i = 0; i < keysA.length; i++) {
-      const key = keysA[i] as keyof T;
-      if (objA[key] !== objB[key]) {
-        return false;
-      }
-    }
-    return true;
+  if (
+    typeof objA !== 'object' ||
+    typeof objB !== 'object' ||
+    objA === null ||
+    objB === null
+  ) {
+    return false;
   }
 
-  return objA === objB;
+  /**
+   * 오브젝트 비교를 위한 객체 생성. 키 순서대로 정렬
+   */
+  const entries = Object.entries;
+  const sort = (entries: [string, any][]) =>
+    entries.sort(([a], [b]) => a.localeCompare(b));
+  const entriesA = sort(entries(objA));
+  const entriesB = sort(entries(objB));
+
+  /**
+   * 키 개수가 다르면 다른 객체
+   */
+  if (entriesA.length !== entriesB.length) {
+    return false;
+  }
+
+  /**
+   * 키와 값이 모두 같은지 비교
+   */
+  for (let i = 0; i < entriesA.length; i++) {
+    const [key, value] = entriesA[i];
+    const [keyB, valueB] = entriesB[i];
+    if (key !== keyB || !Object.is(value, valueB)) {
+      return false;
+    }
+  }
+
+  return true;
 }
