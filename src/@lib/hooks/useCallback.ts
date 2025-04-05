0d411da69a4916b00678ab91b041815a85d38f8f
codeming <hmk2000015@gmail.com>
2025-04-05 22:25:18 +0900

    Update useCallback.ts

--- src/@lib/hooks/useCallback.ts
+++ src/@lib/hooks/useCallback.ts
@@ -1,10 +1,16 @@
 /* eslint-disable @typescript-eslint/no-unused-vars,@typescript-eslint/no-unsafe-function-type */
 import { DependencyList } from 'react';
+import { useRef } from './useRef';
+import { shallowEquals } from '../equalities';
 
 export function useCallback<T extends Function>(
   factory: T,
   _deps: DependencyList
 ) {
   // 직접 작성한 useMemo를 통해서 만들어보세요.
-  return factory as T;
+  const ref = useRef<{ value: T; deps: DependencyList } | null>(null);
+  if (ref.current === null || !shallowEquals(ref.current.deps, _deps)) {
+    ref.current = { value: factory, deps: _deps };
+  }
+  return ref.current.value;
 }
